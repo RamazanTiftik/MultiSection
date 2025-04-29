6fbfe0482469c607bf30afa4d94d54cbe306aba6
Ramazan Tiftik <ramazantiftik3@gmail.com>
2025-04-29 15:06:55 +0300

    nav implemention is done

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AIChatScreen from "./screen/AIChatScreen"
+import MyProfileScreen from "./screen/MyProfileScreen"
+import { Ionicons } from '@expo/vector-icons';
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName='Home'
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = route.name === 'Home' ? 'home' : 'settings';
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#007AFF',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={AIChatScreen} />
+        <Tab.Screen name="MyProfile" component={MyProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screen/AIChatScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AIChatScreen = () => {
+  return (
+    <View>
+      <Text>AIChatScreen</Text>
+    </View>
+  )
+}
+
+export default AIChatScreen
+
+const styles = StyleSheet.create({})
